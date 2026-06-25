709fa690658feebfcd4c65db44bf082b83d369fb
Dika Armansyah <dikarmansyah@gmail.com>
2026-06-26 00:23:53 +0700

    add counter

--- 1-klik.bat
+++ 1-klik.bat
@@ -3,25 +3,22 @@ echo ===================================================
 echo 🚀 MEMULAI PROSES UPDATE SINTELIS UTILITY
 echo ===================================================
 
-echo.
-echo [1/5] Menarik update terbaru dari GitHub...
-git pull origin main
 
 echo.
-echo [2/5] Merakit ulang Docker Image...
+echo [1/4] Merakit ulang Docker Image...
 docker build -t asia-southeast2-docker.pkg.dev/ganti-nama-file/sintelis-repo/sintelis-utility:latest .
 
 echo.
-echo [3/5] Mengunggah ke Google Cloud Registry...
+echo [2/4] Mengunggah ke Google Cloud Registry...
 docker push asia-southeast2-docker.pkg.dev/ganti-nama-file/sintelis-repo/sintelis-utility:latest
 
 echo.
-echo [4/5] Menerbangkan ke Google Cloud Run...
+echo [3/4] Menerbangkan ke Google Cloud Run...
 call gcloud run deploy sintelis-utility --image asia-southeast2-docker.pkg.dev/ganti-nama-file/sintelis-repo/sintelis-utility:latest --region asia-southeast2 --allow-unauthenticated
 
 echo.
-echo [5/5] Memulai Hosting Firebase...
-call firebase deploy --only hosting
+echo [4/4] Memulai Hosting Firebase...
+call firebase deploy --only hosting --project ganti-nama-file
 
 echo.
 echo ===================================================
